3e11f82fc90864e7c59d8bb68864bc7362048011
liugx <18379177354@163.com>
2019-02-15 11:27:49 +0800

    feature: 圆形

--- echart/src/components/HelloWorld.vue
+++ echart/src/components/HelloWorld.vue
@@ -10,31 +10,56 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-
     }
   },
   methods: {
     drawLine () {
-      var myChart = this.$echarts.init(document.getElementById('main'))
-      var option = {
-        title: {
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-          data: ['销量']
-        },
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
+      let myChart = this.$echarts.init(document.getElementById('main'))
+      // var option = {
+      //   title: {
+      //     text: 'ECharts 入门示例'
+      //   },
+      //   tooltip: {},
+      //   legend: {
+      //     data: ['销量']
+      //   },
+      //   xAxis: {
+      //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      //   },
+      //   yAxis: {},
+      //   series: [{
+      //     name: '销量',
+      //     type: 'bar',
+      //     data: [5, 20, 37, 10, 10, 20]
+      //   }]
+      // }
+      let option = {
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            roseType: 'angle',
+            itemStyle: {
+            // 阴影的大小
+            shadowBlur: 100,
+            // 阴影水平方向上的偏移
+            shadowOffsetX: 0,
+            // 阴影垂直方向上的偏移
+            shadowOffsetY: 0,
+            // 阴影颜色
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+            },
+            data: [
+              {value: 235, name: '视频广告'},
+              {value: 274, name: '联盟广告'},
+              {value: 310, name: '邮件营销'},
+              {value: 335, name: '直接访问'},
+              {value: 400, name: '搜索引擎'}
+            ]
+          }
+        ]
       }
-
       myChart.setOption(option)
     }
   },
